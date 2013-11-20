d87fc6694331e3ceb9f0457d965c1dd3b8d54dea
Dan S Turner <turner@nowwithmoredan.com>
2013-11-20 10:40:26 -0800

    Add views.

--- basics.rb
+++ basics.rb
@@ -15,3 +15,27 @@ end
 get '/more/*/*' do
   "multiple parameters: #{params[:splat][0]}, #{params[:splat][1]}"
 end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said #{params[:message]}"
+end
+
+get '/secret' do
+  erb :secret
+end
+
+post '/secret' do
+  "Totally securely encrypted: #{params[:secret].reverse}"
+end
+
+get '/decrypt/:secret' do
+  "Decrypted message: #{params[:secret].reverse}"
+end
+
+not_found do
+  halt 404, 'not found'
+end
