7ead2d45cfda835df2f1deb53883c82a7ec3239e
Dan S Turner <turner@nowwithmoredan.com>
2013-11-20 10:12:10 -0800

    Add pages which use parameters.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 learn-sinatra
 =============
+Working through http://net.tutsplus.com/tutorials/ruby/singing-with-sinatra/ to learn sinatra.
--- basics.rb
+++ basics.rb
@@ -7,3 +7,11 @@ end
 get '/about' do
   "A little about me."
 end
+
+get '/about/:name/:city' do
+  "Hello #{params[:name]} from #{params[:city]}"
+end
+
+get '/more/*/*' do
+  "multiple parameters: #{params[:splat][0]}, #{params[:splat][1]}"
+end
